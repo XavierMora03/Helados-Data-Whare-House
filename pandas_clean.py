e76f15565301e9c697a7f9828bb52a17d251010f
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-11-24 16:44:57 -0600

    pandas_clean looking better, ' character matched better

--- pandas_clean.py
+++ pandas_clean.py
@@ -7,7 +7,7 @@ reviews_text_labels = ['text','title']
 products_text_labels = ['name','subhead','description','ingredients']
 
 reviews.date = pd.to_datetime(reviews.date, format='%Y-%m-%d')
-
+pd.set_option('display.max_colwidth', None)
 #only words and spaces
 def tolower(df,label):
         df[label] = df[label].str.lower()
@@ -20,8 +20,11 @@ def transformReviews(df,lables):
     for t in lables:
         df = tolower(df,t)
         # df[t] = df[t].str.replace("[^\w\s\']",'',regex=True)
-        df[t] = df[t].str.replace(r"['-_\|//’]",' ',regex=True)
-        df[t] = df[t].str.replace(r"[-.?'!,:;()|0-9]",'',regex=True)
+        # df[t] = df[t].str.replace(r'([a-z])\1+', r'\1', regex=True)
+        # df[t] = df[t].str.replace(r"['-_/\|’]",' ',regex=True)
+        df[t] = df[t].str.replace("[^\w\s'’]",'',regex=True)
+        df[t] = df[t].str.replace(r"[\\\\]",'',regex=True)
+        # df[t] = df[t].str.replace(r"[-.?'!,:;()|0-9]",'',regex=True)
 
     
     return df
@@ -34,6 +37,7 @@ def transformProducts(df,lables):
     
     return df
 
+print(reviews.text[10:15])
 
 reviews = transformReviews(reviews,reviews_text_labels)
 reviews = tolower(reviews,'likes')
@@ -42,5 +46,6 @@ products = transformProducts(products,products_text_labels)
 reviews.index.name = 'id'
 reviews.reset_index(inplace=True)
 
+print("\n\n\n\n",reviews.text[10:15].to_string())
 products.to_csv('products_clean.csv',index=False)
 reviews.to_csv('reviews_clean.csv',encoding='utf-8-sig',index=False)
