73d28d5d44ffd7aebd17e28a3434ec2d8989c9f4
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-11-24 18:05:00 -0600

    naturalLanguageProcessing.py cleaned, and correctly lemmatize

--- naturalLanguageProcessing.py
+++ naturalLanguageProcessing.py
@@ -2,7 +2,6 @@
 #outputs lematisations of a proper word
 #gone going should be maped into go
 import pandas as pd
-import nltk
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 from nltk.stem import WordNetLemmatizer
@@ -15,12 +14,13 @@ import itertools
 stem = WordNetLemmatizer()
 reviews = pd.read_csv('reviews_clean.csv')
 # no_punctuation_regex = r'[-.?!,:;()|0-9]'
-punctuation = re.compile(r'-.?!,:;()|0-9')
-
+# punctuation = re.compile(r"[-.?!,:;'()|0-9]")
+punctuation = re.compile(r"['’]")
 stop_words = set(stopwords.words('english'))
-print(stop_words,'\n\n\n')
-# stop_words.add("'")
-# stop_words.add("’")
+
+mystop_words = set([punctuation.sub('',w) for w in stop_words ])
+stop_words = stop_words.union(mystop_words)
+
 def convert_word(word):
     if word in words.words():
         return word
@@ -28,16 +28,13 @@ def convert_word(word):
         return re.sub(r'([a-z])\1+', r'\1', word)
     
 
-print("n't" in stop_words)
 def cleanStringFile(row):
     if(row.text is np.nan):
         return []
 
     text_tokens = word_tokenize(row.text, language='english') 
     text_tokens = [t for t in text_tokens if  t not in stop_words and len(t) > 2 and  not t.__contains__("'")]
-    text_tokens = [stem.lemmatize(t) for t in text_tokens]
-    # text_tokens = [t for t in text_tokens if  t not in stop_words and len(t) > 2]
-    # text_tokens = [x for x in text_tokens if x in words.words()]
+    text_tokens = [stem.lemmatize(t,'v') for t in text_tokens]
     fdist = FreqDist()
 
     for t in text_tokens:
