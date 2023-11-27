e1800b85d5a3fa7a9d3c12455c40a569fa90f3c7
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-11-26 22:02:08 -0600

    optimized and cleaned the ingredients_words_per_review.py

--- ingredients_words_per_review.py
+++ ingredients_words_per_review.py
@@ -11,13 +11,11 @@ from nltk.corpus import stopwords, words
 import re
 import itertools
 
-pd.set_option('display.max_columns', None)
 stem = WordNetLemmatizer()
 reviews = pd.read_csv('reviews_clean.csv')
 products = pd.read_csv('products_clean.csv')
-# no_punctuation_regex = r'[-.?!,:;()|0-9]'
-# punctuation = re.compile(r"[-.?!,:;'()|0-9]")
 punctuation = re.compile(r"['’]")
+
 stop_words = set(stopwords.words('english'))
 
 mystop_words = set([punctuation.sub('',w) for w in stop_words ])
@@ -37,28 +35,16 @@ def cleanStringFile(row):
     text_tokens = word_tokenize(row.text, language='english') 
     text_tokens = [t for t in text_tokens if  t not in stop_words and len(t) > 2 and  not t.__contains__("'")]
     text_tokens = [stem.lemmatize(t,'v') for t in text_tokens]
-    # fdist = FreqDist()
-    #
-    # for t in text_tokens:
-    #     fdist[t] +=1
-    # 
-    return list([row.id,row.key, ','.join(text_tokens), row.stars])
-    return list([row.id,row.key, ' '.join(fdist.elements()), row.stars])
 
+    return list([row.id,row.key, ','.join(text_tokens), row.stars])
 
 
 #so it does not return a pd.series
 list_train_data = list(reviews.apply(cleanStringFile, axis=1))
-
-
 reviews_transformed = pd.DataFrame(list_train_data, columns = ['id','key','words','stars'])
-
-products.key = products.key.astype(str)
-reviews_transformed.key = reviews_transformed.key.astype(str)
 
 reviews_transformed =  reviews_transformed.merge(products[['key','ingredients']], how='left', on='key')
 reviews_transformed = reviews_transformed.drop(labels = ['id','key'],axis=1)
-reviews_transformed.info()
 
 reviews_transformed.to_csv('ingredientes-words_per_review.csv',index=False)
 
