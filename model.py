650a844366e6e1712c987fb7425138aae4817281
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-11-27 00:07:32 -0600

    ready for onehotencoder

--- model.py
+++ model.py
@@ -9,6 +9,4 @@ labels = ['words','ingredients']
 for l in labels:
     reviews[l] = reviews[l].map(lambda x:  np.array(x.split(','),dtype='S'))
 
-# reviews = reviews.dropna(how='any',axis=0)
-
 print(reviews.loc[0])
