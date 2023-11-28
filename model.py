879b728426f98c35912e4cbb85160d9cbaa570be
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-11-27 20:35:23 -0600

    muchos comentarios

--- model.py
+++ model.py
@@ -9,13 +9,15 @@ from sklearn.pipeline import make_pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import average_precision_score
 
+from sklearn.svm import LinearSVC
+
 df = pd.read_csv('./ingredientes_words_per_review.csv',encoding='utf-8')
 
 labels = ['words','ingredients']
 
 for l in labels:
     # df[l] = df[l].map(lambda x:  np.array(x.split(','),dtype='S'))
-    df[l] = df[l].astype('S')
+    df[l] = df[l].astype(str)
 
 
 #words
@@ -24,30 +26,39 @@ cv = CountVectorizer()
 #stars
 scale = MinMaxScaler()
 
-df.ingredients = cv.fit_transform(df.ingredients)
-df.words = Tfid.fit_transform(df.words)
+# df.ingredients = cv.fit_transform(df.ingredients)
+# df.words = Tfid.fit_transform(df.words)
 
-df.stars = scale.fit_transform(df[['stars']])
+# df.stars = df.stars.astype(float)
 
-print(df.stars)
+# df.stars = scale.fit_transform(df[['stars']])
+# df.words = Tfid.fit_transform(np.array(df.words))
+# df.ingredients = cv.fit_transform(df.ingredients)
 
 preprocessing = ColumnTransformer([
-    ("ingredients", make_pipeline(cv), ['ingredients']),
-    ("words", make_pipeline(Tfid), ['words']),
+    ("ingredients", cv, ['ingredients']),
+    ("words", Tfid, ['words']),
     ])
     # ("stars",MinMaxScaler(), ['stars'])
     # ])
 
+# print(df)
 dd = preprocessing.fit_transform(df)
+print(dd.shape())
+quit() 
+dd = pd.DataFrame(dd, columns=df.getfe)
 df_x = df[['ingredients','stars']]
 df_y = df['words']
 
 
+
 x_train, x_test, y_train, y_test = train_test_split(df_x,df_y, test_size = 0.2, random_state=4)
 
+print(df_x)
+
 # x_trainCv = cv.fit_transform(x_train['ingredients'])
-print(cv.get_feature_names_out())
-mnb = MultinomialNB()
+# print(cv.get_feature_names_out())
+mnb = LinearSVC()
 mnb.fit(x_train, y_train)
 predictions = mnb.predict(x_test)
 score = average_precision_score(y_test,predictions)
